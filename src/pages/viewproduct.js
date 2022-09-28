938724cbbb6e3681762b10d46629a28eebad0de7
Parth Barai <parthnbarai1234@gmail.com>
2022-09-28 20:04:35 +0530

    login quantity done

--- src/pages/viewproduct.js
+++ src/pages/viewproduct.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 import logo from '../assets/images/loginimg.png'
 import Carousal from './carousal'
 import Button from 'react-bootstrap/Button';
 import '../components/product.css'
+import add from '../assets/images/add.png'
+import sub from '../assets/images/subtract.png'
 
 
-function viewproduct(props) {
+function Viewproduct(props) {
+    const [item, setItem] = useState(1)
+
+    const addItem = () => {
+        setItem((item) => parseInt(item) + 1)
+    }
+    const subtractItem = () => {
+        if (item > 1) {
+            setItem((item) => parseInt(item) - 1)
+        }
+    }
+
     return (
         <>
             <div className="container my-3">
@@ -21,10 +34,32 @@ function viewproduct(props) {
                         <div className="my-3">
                             {props.price}
                         </div>
-                        <div className="my-3">
-                            {props.quantity}
+                        <div className="row">
+                            <div className="col-1">
+                                <img
+                                    src={add}
+                                    width="25"
+                                    height="25"
+                                    className="d-inline-block align-top"
+                                    alt="Brand logo"
+                                    onClick={() => addItem()}
+                                />
+                            </div>
+                            <div className="col-1">
+                                {item}
+                            </div>
+                            <div className="col-1">
+                                <img
+                                    src={sub}
+                                    width="25"
+                                    height="25"
+                                    className="d-inline-block align-top"
+                                    alt="Brand logo"
+                                    onClick={() => subtractItem()}
+                                />
+                            </div>
                         </div>
-                        <div className='align-button'>
+                        <div className='align-button my-3'>
                             <div className="col md-3">
                                 <Button variant="success">Buy</Button>
                             </div>
@@ -58,4 +93,4 @@ function viewproduct(props) {
     )
 }
 
-export default viewproduct
+export default Viewproduct
